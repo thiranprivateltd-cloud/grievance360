c724e84d8dbcac35b846569bf386152f80456550
thiranprivateltd-cloud <thiranprivateltd@gmail.com>
2026-08-19 11:56:52 +0530

    Update Footer.jsx

--- stage2-fullstack/frontend/src/components/Footer.jsx
+++ stage2-fullstack/frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="py-3 text-center text-secondary bg-dark border-top border-secondary mt-auto">
       <div className="container">
-        <small className="text-secondary opacity-75">Designed & Developed by Varshith G S</small>
+        <small className="text-secondary opacity-75">Designed & Developed by Mukunthan and Team</small>
       </div>
     </footer>
   );
